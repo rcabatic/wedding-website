246513040007e7fc8e52214f649721bf6d70c18a
Richelle Cabatic <rcabatic11@gmail.com>
2020-03-19 20:58:54 +0000

    add navbar and packages

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ var express 	= require("express"),
 	  mongoose 	= require("mongoose"),
 	  bodyParser 	= require("body-parser"),
 	  mongoose 	= require("mongoose"),
+	methodOverride			= require("method-override"),
 	  flash		= require("connect-flash");
-
-app.set("view engine", "ejs");
 
 var url = process.env.DATABASEURL || "mongodb://localhost:27017/wedding"; //DEFAULT vals
 console.log(process.env.DATABASEURL); //shows you which one..
@@ -19,6 +18,22 @@ mongoose.connect( url , {
 	console.log("Connected to Mongo Atlas DB!");
 }).catch(err => {
 	console.log("Error! : ", err.message)
+});
+
+//Need Passports Config when set up password
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + "/public")); //safer if directory changed., for stylesheet. 
+app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
+//app.use(flash()); //flash messages.
+
+//Call function on every single route.
+app.use(function(req, res, next){
+	//res.locals.currentUser = req.user;
+	//res.locals.error = req.flash("error");
+	//res.locals.success = req.flash("success");
+	next(); //very important
 });
 
 //==ROUTES== 
