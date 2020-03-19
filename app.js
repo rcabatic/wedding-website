c5cda4cf9a597db2aa34f521c5a557b3da5ff99f
Richelle Cabatic <rcabatic11@gmail.com>
2020-03-19 20:10:10 +0000

    added env vars for db

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
+var express 	= require("express"),
+	  app 		= express(),
+	  mongoose 	= require("mongoose"),
+	  bodyParser 	= require("body-parser"),
+	  mongoose 	= require("mongoose"),
+	  flash		= require("connect-flash");
 
 app.set("view engine", "ejs");
 
-mongoose.connect("mongodb+srv://rcabatic:Arseny@clusterar-ayekl.mongodb.net/test?retryWrites=true&w=majority", {
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/wedding"; //DEFAULT vals
+console.log(process.env.DATABASEURL); //shows you which one..
+
+
+mongoose.connect( url , {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useUnifiedTopology: true
 }).then(() =>{
 	console.log("Connected to Mongo Atlas DB!");
 }).catch(err => {
-	console.log("Error! : ", err)
+	console.log("Error! : ", err.message)
 });
 
 //==ROUTES== 
