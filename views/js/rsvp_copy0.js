f374eda86d4829b9460262a5d109265dcfbf5135
Richelle Cabatic <rcabatic11@gmail.com>
2020-04-14 21:29:23 +0000

    updated pages

---
+++ views/js/rsvp_copy0.js
@@ -0,0 +1,31 @@
+alert("Hello?");
+
+$("#joinUs").change(function() {
+  if ($(this).val() == "no") {
+    $('#rejectGroup').show();
+    // $('#otherField').attr('required', '');
+    // $('#otherField').attr('data-error', 'This field is required.');
+  } else {
+    $('#acceptGroup').hide();
+    // $('#otherField').removeAttr('required');
+    // $('#otherField').removeAttr('data-error');
+  }
+});
+$("#joinUs").trigger("change");
+
+$("#seeAnotherFieldGroup").change(function() {
+  if ($(this).val() == "yes") {
+    $('#otherFieldGroupDiv').show();
+    $('#otherField1').attr('required', '');
+    $('#otherField1').attr('data-error', 'This field is required.');
+    $('#otherField2').attr('required', '');
+    $('#otherField2').attr('data-error', 'This field is required.');
+  } else {
+    $('#otherFieldGroupDiv').hide();
+    $('#otherField1').removeAttr('required');
+    $('#otherField1').removeAttr('data-error');
+    $('#otherField2').removeAttr('required');
+    $('#otherField2').removeAttr('data-error');
+  }
+});
+$("#seeAnotherFieldGroup").trigger("change");
